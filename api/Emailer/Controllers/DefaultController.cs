cd0e43e706c1efba9d2cabae339fd35bf34bb1b2
Edwin Gustafson <edwingustafson@gmail.com>
2021-03-05 12:53:54 -0500

    Default controller redirects to Swagger UI

---
+++ api/Emailer/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emailer.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("/")]
+        [HttpHead("/")]
+        public IActionResult Index()
+        {
+            return new RedirectResult("~/swagger/index.html");
+        }
+    }
+}
